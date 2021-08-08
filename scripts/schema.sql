579bd571d6e3c93da3c5b69bb3afd470a309f8ea
xndong1020 <xndong1020@gmail.com>
2021-08-08 12:13:45 +1000

    bug fixings

--- scripts/schema.sql
+++ scripts/schema.sql
@@ -6,14 +6,15 @@ CREATE TABLE `translation` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `createdAt` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
   `updatedAt` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
-  `keyName` varchar(255) NOT NULL,
-  `en` varchar(255) DEFAULT NULL,
-  `fr` varchar(255) DEFAULT NULL,
-  `zh` varchar(255) DEFAULT NULL,
-  `pt` varchar(255) DEFAULT NULL,
-  `es` varchar(255) DEFAULT NULL,
-  `ar` varchar(255) DEFAULT NULL,
-  `ko` varchar(255) DEFAULT NULL,
+  `keyName` text NOT NULL,
+  `projectName` text NOT NULL,
+  `en` text DEFAULT NULL,
+  `fr` text DEFAULT NULL,
+  `zh` text DEFAULT NULL,
+  `pt` text DEFAULT NULL,
+  `es` text DEFAULT NULL,
+  `ar` text DEFAULT NULL,
+  `ko` text DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- src/entity/Translation.ts
+++ src/entity/Translation.ts
@@ -1,32 +1,32 @@
-import { Entity, Column } from "typeorm";
-import { CoreEntity } from "./CoreEntity";
+import { Entity, Column } from 'typeorm'
+import { CoreEntity } from './CoreEntity'
 
 @Entity()
 export class Translation extends CoreEntity {
   @Column()
-  projectName: string;
+  projectName: string
 
   @Column()
-  keyName: string;
+  keyName: string
 
-  @Column({ nullable: true })
-  en: string;
+  @Column('text', { nullable: true })
+  en: string
 
-  @Column({ nullable: true })
-  fr: string;
+  @Column('text', { nullable: true })
+  fr: string
 
-  @Column({ nullable: true })
-  zh: string;
+  @Column('text', { nullable: true })
+  zh: string
 
-  @Column({ nullable: true })
-  pt: string;
+  @Column('text', { nullable: true })
+  pt: string
 
-  @Column({ nullable: true })
-  es: string;
+  @Column('text', { nullable: true })
+  es: string
 
-  @Column({ nullable: true })
-  ar: string;
+  @Column('text', { nullable: true })
+  ar: string
 
-  @Column({ nullable: true })
-  ko: string;
+  @Column('text', { nullable: true })
+  ko: string
 }
